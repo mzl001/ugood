f173bbfbe96acff00339054c722933b665b0256e
mzl001 <1276581914@qq.com>
2018-04-27 11:12:23 +0800

    init

---
+++ src/main/java/com/ddwmn/DdwmnApplication.java
@@ -0,0 +1,12 @@
+package com.ddwmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdwmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdwmnApplication.class, args);
+	}
+}
